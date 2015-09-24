16ba18b068e2ca683bb9420be5bbe8495c03b5d0
ishwariShevade <ishwari.she@gmail.com>
2015-09-24 14:49:01 -0700

    Created Splash screen

---
+++ Profiler/app/src/main/java/com/example/ishwari/profiler/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.ishwari.profiler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by ishwari on 9/24/15.
+ */
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        finish();
+    }
+}
+
+
