b56dda2d986ad7a06c9bdcc72eeb63dcc748d604
ishwariShevade <ishwari.she@gmail.com>
2015-10-14 21:03:26 -0700

    Added Snackbar to display current location marker onClick shows location Location gets updated after marker is dragged

--- Profiler/app/src/main/java/com/example/ishwari/profiler/MapViewer.java
+++ Profiler/app/src/main/java/com/example/ishwari/profiler/MapViewer.java
@@ -1,49 +1,134 @@
 package com.example.ishwari.profiler;
 
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
-import android.app.Activity;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by siddhartha on 9/30/15.
  */
 
-
-
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MapViewer extends FragmentActivity {
+public class MapViewer extends FragmentActivity implements LocationListener{
 
     private GoogleMap mMap;
+    Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!isGooglePlayServicesAvailable()) {
+            finish();
+        }
+
         setContentView(R.layout.map_view);
-        setUpMapIfNeeded();
+        //setUpMapIfNeeded();
+
+        SupportMapFragment supportMapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMap = supportMapFragment.getMap();
 
         mMap.setMyLocationEnabled(true);
-        mMap.getMyLocation();
+        //mMap.getMyLocation();
 
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+        locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
 
-        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(37.721897, -122.47820939999997));
-        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+        //CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(37.721897, -122.47820939999997));
+        //CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
 
-        mMap.moveCamera(center);
-        mMap.animateCamera(zoom);
+        //mMap.moveCamera(center);
+        //mMap.animateCamera(zoom);
 
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView locationTv = (TextView) findViewById(R.id.latlongLocation);
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        //mMap.addMarker(new MarkerOptions().position(latLng).draggable(true).flat(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        marker=mMap.addMarker(new MarkerOptions().position(latLng)
+                .title("You are here")
+                .snippet(String.valueOf(latLng))
+                .draggable(true));
+                //.icon(BitmapDescriptorFactory.fromResource(R.drawable.mark_start)));
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+
+            @Override
+            public void onMarkerDrag(Marker arg0) {
+                // TODO Auto-generated method stub
+                Log.d("Marker", "Dragging");
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker arg0) {
+                // TODO Auto-generated method stub
+                LatLng markerLocation = marker.getPosition();
+                Toast.makeText(getApplicationContext(), markerLocation.toString(), Toast.LENGTH_LONG).show();
+                marker.setSnippet(String.valueOf(markerLocation));
+                Log.d("Marker", "finished");
+            }
+
+            @Override
+            public void onMarkerDragStart(Marker arg0) {
+                // TODO Auto-generated method stub
+                Log.d("Marker", "Started");
+
+            }
+        });
+        locationTv.setText("Latitude:" + latitude + ", Longitude:" + longitude);
     }
 
     @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == status) {
+            return true;
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
+            return false;
+        }
+    }
+    /*@Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
@@ -64,7 +149,7 @@ public class MapViewer extends FragmentActivity {
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
      * method in {@link #onResume()} to guarantee that it will be called.
      */
-    private void setUpMapIfNeeded() {
+   /* private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
@@ -83,10 +168,13 @@ public class MapViewer extends FragmentActivity {
      * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(37.721897, -122.47820939999997)).title("You are here").flat(true).draggable(true));
+   /* private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.721897, -122.47820939999997))
+                .title("You are here")
+                .flat(true).draggable(true));
 
-    }
+
+    }*/
 
 
     }
