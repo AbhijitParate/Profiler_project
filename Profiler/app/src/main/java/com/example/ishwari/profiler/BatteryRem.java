81cce9e5f7045f9946b4163884af44229b00fed0
SIDDHARTHA <ssiddhartha.1193@gmail.com>
2015-10-22 15:14:24 -0700

    Added Dialog on click to show preferences

--- Profiler/app/src/main/java/com/example/ishwari/profiler/BatteryRem.java
+++ Profiler/app/src/main/java/com/example/ishwari/profiler/BatteryRem.java
@@ -2,10 +2,23 @@ package com.example.ishwari.profiler;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.PopupWindow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by siddhartha on 9/30/15.
@@ -14,10 +27,13 @@ public class BatteryRem extends Activity {
 
     private SeekBar sb;
     private TextView textView;
+    private PopupWindow popup;
+    private Button button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.battery_rem);
@@ -25,37 +41,78 @@ public class BatteryRem extends Activity {
         sb = (SeekBar) findViewById(R.id.seekBar);
         textView = (TextView) findViewById(R.id.textView1);
 
-        textView.setText("Covered: " + sb.getProgress() + "/" + sb.getMax());
 
-        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            int progress = 0;
-
-
-
+        textView.setText("Selected: " + sb.getProgress() + "%");
+        button= (Button)findViewById(R.id.button_set);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-
-            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
-
-                progress = progresValue;
+            public void onClick(View v) {
 
 
+                // custom dialog
+                final Dialog dialog = new Dialog(BatteryRem.this, R.style.AppTheme);
+                dialog.setContentView(R.layout.preferences);
+                dialog.setTitle("Preferences");
+
+
+                GridView gridView = (GridView) dialog.findViewById(R.id.gridView);
+
+                gridView.setAdapter(new GridViewAdapter(getApplication()));
+                gridView.setNumColumns(4);
+                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // do something here
+                    }
+                });
+                dialog.show();
+                Button closeButton = (Button)dialog.findViewById(R.id.button_cancel);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+              //
             }
-
-            @Override
-
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                textView.setText("Covered: " + progress + "/" + seekBar.getMax());
-
-
-            }
-
         });
 
-    }
+
+    sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+
+                                  {
+
+                                      int progress = 0;
+
+
+                                      @Override
+
+                                      public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+
+                                          progress = progresValue;
+
+
+                                      }
+
+                                      @Override
+
+                                      public void onStartTrackingTouch(SeekBar seekBar) {
+
+
+                                      }
+
+                                      @Override
+                                      public void onStopTrackingTouch(SeekBar seekBar) {
+                                          textView.setText("Selected: " + progress + "%");
+
+
+                                      }
+
+                                  }
+
+    );
+
 }
+
+    }
