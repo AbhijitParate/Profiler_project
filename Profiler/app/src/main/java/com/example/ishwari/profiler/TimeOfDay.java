74cab0b7b84a98b791fc1cafca55bbed670c9732
SIDDHARTHA <ssiddhartha.1193@gmail.com>
2015-12-02 18:32:26 -0800

    Added toggle buttons to grid view

--- Profiler/app/src/main/java/com/example/ishwari/profiler/TimeOfDay.java
+++ Profiler/app/src/main/java/com/example/ishwari/profiler/TimeOfDay.java
@@ -1,18 +1,28 @@
 package com.example.ishwari.profiler;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by siddhartha on 9/30/15.
@@ -26,6 +36,16 @@ public class TimeOfDay extends MainActivity {
     TextView time_2;
     Calendar calendar;
     String format = "";
+    Button set_mode;
+    int hour;
+    int min;
+    private AudioManager audio;
+    private PendingIntent pendingIntentam;
+    private PendingIntent pentdingIntentpm;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
+    private WifiManager wifi;
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +55,7 @@ public class TimeOfDay extends MainActivity {
         timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
         time = (TextView) findViewById(R.id.textView1);
         time_2 = (TextView) findViewById(R.id.textView2);
+
         calendar = Calendar.getInstance();
 
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
@@ -73,8 +94,30 @@ public class TimeOfDay extends MainActivity {
         }
         time.setText(new StringBuilder().append(" From: ").append(hour).append(" : ").append(min)
                 .append(" ").append(format));
-        time_2.setText(new StringBuilder().append(" From: ").append(hour).append(" : ").append(min)
-                .append(" ").append(format));
+
+
+    }
+
+    public void scheduleAlarm(View V)
+    {
+
+        // time at which alarm will be scheduled here alarm is scheduled at 1 day from current time,
+        // we fetch  the current time in milliseconds and added 1 day time
+        // i.e. 24*60*60*1000= 86,400,000   milliseconds in a day
+        Long time = new GregorianCalendar().getTimeInMillis()+1*60*1000;
+
+        // create an Intent and set the class which will execute when Alarm triggers, here we have
+        // given AlarmReciever in the Intent, the onRecieve() method of this class will execute when
+        // alarm triggers and
+        //we will write the code to send SMS inside onRecieve() method pf Alarmreciever class
+        Intent intentAlarm = new Intent(this, AlarmReciever.class);
+
+        // create the object
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        //set the alarm for particular time
+        alarmManager.set(AlarmManager.RTC_WAKEUP,time, PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
+        Toast.makeText(TimeOfDay.this, "Settings will be activated in a minute", Toast.LENGTH_LONG).show();
 
     }
 }
